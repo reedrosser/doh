bb519fcd1a2180a8c244f352dfa16625990b9d0c
Reed Rosser <artrosse@WKMUS1565316.global.publicisgroupe.net>
2019-03-27 23:22:18 -0400

    Symbols provided by CSV have carets rather than dashes. Regex replaces them.

--- src/services/pricehistory.js
+++ src/services/pricehistory.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export function getHistory(value, callback) {
+  const val = value.replace(/\^/g, "-");
   axios
     .get(
-      `https://api.iextrading.com/1.0/stock/${value}/chart/1m?filter=high,low,open,close,date`
+      `https://api.iextrading.com/1.0/stock/${val}/chart/1m?filter=high,low,open,close,date`
     )
     .then(res => {
       callback(res.data);
